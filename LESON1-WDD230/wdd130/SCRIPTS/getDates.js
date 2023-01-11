b4e67cb52d43b65443a7ab9cdf1c4c99771ac297
Romi27 <97213039+Romi27@users.noreply.github.com>
2023-01-11 12:41:28 -0300

    Delete LESON1-WDD230/wdd130 directory

--- LESON1-WDD230/wdd130/SCRIPTS/getDates.js
+++
@@ -1,5 +0,0 @@
-var currentDate =  new Date()
-var cYear= currentDate.getFullYear()
-document.getElementById("currentYear").innerHTML = cYear;
-let nLastModif = new Date(document.lastModified);
-document.getElementById("lastModified").innerHTML = nLastModif;
