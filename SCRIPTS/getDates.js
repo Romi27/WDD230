fedb83e6f3c60f3afd4991987f9d0ce5b8012d99
Romina <romialdana27@gmail.com>
2023-01-12 23:20:30 -0300

    part2

--- SCRIPTS/getDates.js
+++ SCRIPTS/getDates.js
@@ -3,11 +3,28 @@ var cYear= currentDate.getFullYear()
 document.getElementById("currentYear").innerHTML = cYear;
 let nLastModif = new Date(document.lastModified);
 document.getElementById("lastModified").innerHTML = nLastModif;
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
+const menubutton = document.querySelector("#menu-button");
+const menuitems = document.querySelectorAll(".menu-item");
+// menuitems will be a Node List of the list items
+
+menubutton.addEventListener("click", () => {
+	menuitems.forEach((item) => item.classList.toggle("open"));
+	menubutton.classList.toggle("close");
+});
+
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("☑️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "❎";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "☑️";
+	}
+});
+
